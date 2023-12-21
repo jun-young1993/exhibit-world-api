fa9647a4e36b9d44c6fa6016004eeba7a065c140
junyoungkim <juny3738@gmail.com>
2023-12-21 22:33:51 +0900

    EWA-10

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { EntityHelper } from "../../utils/entity-helper";
 import { ApiProperty } from "@nestjs/swagger";
 import { Mesh } from "../../meshes/entities/mesh.entity";
@@ -8,6 +8,36 @@ export class Group extends EntityHelper{
   @ApiProperty({ example: 'cbcfa8b8-3a25-4adb-a9c6-e325f0d0f3ae' })
   @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column('float',{scale: 5, default: 0})
+  positionX: number;
+
+  @Column('float',{scale: 5, default: 0})
+  positionY: number;
+
+  @Column('float',{scale: 5, default: 0})
+  positionZ: number;
+
+  @Column('float',{scale: 5, default: 0})
+  rotationX: number;
+
+  @Column('float',{scale: 5, default: 0})
+  rotationY: number;
+
+  @Column('float',{scale: 5, default: 0})
+  rotationZ: number;
+
+  @Column('float',{scale: 5, default: 0})
+  quaternionX: number;
+
+  @Column('float',{scale: 5, default: 0})
+  quaternionY: number;
+
+  @Column('float',{scale: 5, default: 0})
+  quaternionZ: number;
+
+  @Column('float',{scale: 5, default: 0})
+  quaternionW: number;
 
   @OneToMany(
     () => Mesh,
